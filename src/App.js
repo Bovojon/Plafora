f31b1d6edd2c732019e854e7ce63ecd4f2ccadda
Bovojon <bovojon@gmail.com>
2020-08-10 16:31:36 -0700

    Add new Home component in App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {history} from './store';
+import {Switch, Route} from 'react-router-dom';
+import {ConnectedRouter} from 'connected-react-router';
+import ReduxToastr from 'react-redux-toastr';
+
+import Home from './components/screen/Home';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <ReduxToastr timeout={4000} transitionIn="fadeIn" transitionOut="fadeOut" />
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route to="/" component={Home} />
+        </Switch>
+      </ConnectedRouter>
     </div>
   );
 }
