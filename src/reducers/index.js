713a62d1f3a8580f2e9173c4468d361297648d85
Bovojon <bovojon@gmail.com>
2020-08-16 13:03:29 -0700

    Add userReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import {reducer as toastrReducer} from 'react-redux-toastr';
 
+import userReducer from './userReducer';
 import authReducer from './authReducer';
 
 export default (history) => combineReducers({
     router: connectRouter(history),
     auth: authReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    user: userReducer
 });
