b9de5fee907b0c7cc971ee853b7811c24e5095e5
Bovojon <bovojon@gmail.com>
2020-08-31 11:11:10 -0700

    Add more actions to userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,17 @@
+import {SIGN_OUT} from '../actions';
+
 const userReducer = (state={}, action) => {
     switch(action.type) {
-        case 'FETCH_USER_SUCCESS':
-            return {...state, ...action.payload}
+        case "SET_USER":
+            return {...state, ...action.payload.user}
+        case "UPDATE_USER_SUCCESS":
+            return {
+                ...state,
+                first_name: action.payload.first_name,
+                last_name: action.payload.last_name
+            }
+        case SIGN_OUT:
+            return null;
         default:
             return state;
     }
