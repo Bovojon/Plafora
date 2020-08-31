12b49e388b095f087e60a5d29992723f49743ee3
Bovojon <bovojon@gmail.com>
2020-08-31 10:14:53 -0700

    Add spaces to be loaded and saved to and from localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,21 @@
 import {createStore, applyMiddleware, compose} from 'redux';
+import {save, load, combineLoads} from 'redux-localstorage-simple';
 import createSagaMiddleware from 'redux-saga';
+import {createLogger} from 'redux-logger';
 import {createBrowserHistory} from 'history';
 import {routerMiddleware} from 'connected-react-router';
-import {createLogger} from 'redux-logger';
 
 import createRootReducer from '../reducers';
-import {save, load, combineLoads} from 'redux-localstorage-simple';
 import rootSaga from '../sagas';
 
-const logger = createLogger();
 export const history = createBrowserHistory();
+const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 
-/*
-- compose() is used to apply multiple store enhancers (e.g. middleware, time 
-    travel, persistence). 
-- applyMiddleware() is the only enhancer that ships with Redux.
-- Redux-LocalStorage-Simple is used to save and load Redux state to and from LocalStorage.
-*/
 const store = createStore(
     createRootReducer(history),
     combineLoads(load({
-        states: ['auth', 'user'],
+        states: ['auth', 'user', 'spaces'],
         namespace: 'plafora'
     })),
     compose(applyMiddleware(
@@ -29,7 +23,7 @@ const store = createStore(
         logger, 
         sagaMiddleware,
         save({
-            states: ['auth', 'user'],
+            states: ['auth', 'user', 'spaces'],
             namespace: 'plafora',
             debounce: 2000
         })
