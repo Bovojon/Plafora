1b96e3ae2f5fe52f1b606ec441134a3d3c1a6ef8
Bovojon <bovojon@gmail.com>
2020-08-31 10:16:35 -0700

    Import placeholder components and set up paths in App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import {history} from './store';
 import {Switch, Route} from 'react-router-dom';
 import {ConnectedRouter} from 'connected-react-router';
 import ReduxToastr from 'react-redux-toastr';
-import {ThemeProvider} from '@material-ui/styles';
-import {createMuiTheme} from '@material-ui/core/styles';
 
 import './App.scss';
+import {history} from './store';
 import Home from './components/screens/Home';
 import Navbar from './components/nav/Navbar';
-
-const theme = createMuiTheme();
+import Login from './components/screens/Login';
+import Register from './components/screens/Register';
+import Spaces from './components/screens/Spaces';
+import Space from './components/screens/Space';
+import Profile from './components/screens/Profile';
+import RequestConfirmation from './components/screens/RequestConfirmation';
+import NotFound from './components/screens/NotFound';
+import ScrollToTop from './components/misc/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <ReduxToastr timeout={4000} transitionIn="fadeIn" transitionOut="fadeOut" />
+      <div className="App">
+        <ReduxToastr timeOut={4000} transitionIn="fadeIn" transitionOut="fadeOut" />
         <ConnectedRouter history={history}>
+          {/* <ScrollToTop /> */}
           <Navbar />
           <Switch>
-            <Route to="/" component={Home} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/places" component={Spaces} />
+            <Route exact path="/place/:id" component={Space} />
+            <Route exact path="/request/:id/confirmation" component={RequestConfirmation} />
+            <Route component={NotFound} />
           </Switch>
         </ConnectedRouter>
-      </ThemeProvider>
-    </div>
+      </div>
   );
 }
 
