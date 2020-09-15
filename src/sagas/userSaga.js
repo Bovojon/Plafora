9b4fdfe94913daa502a118cd1fe84fb5955854d6
Bovojon <bovojon@gmail.com>
2020-09-14 23:40:48 -0700

    Complete updateUser saga

--- src/sagas/userSaga.js
+++ src/sagas/userSaga.js
@@ -1,27 +1,51 @@
 import {all, call, takeEvery, put} from 'redux-saga/effects';
-import {handleErrorSaga} from './errorSaga';
-import {UserService} from '../services';
+import { push } from 'connected-react-router';
+import { toastr } from 'react-redux-toastr';
 
-// Worker
-function* handleFetchUserSaga() {
+import { handleErrorSaga, handleApiErrorSaga } from './errorSaga';
+import { UserService } from '../services';
+
+function* udpateUser({ payload: { values, setSubmitting, resetForm, setErrors, isTouched } }) {
+    const form_first_name = values.first_name;
+    const form_last_name = values.last_name;
+
     try {
-        const response = yield call(UserService.getUser);
-        const responseData = yield response.data;
-        yield put({type: 'FETCH_USER_SUCCESS', payload: responseData});
-    } catch(error) {
-        yield call(handleErrorSaga, error);
+        const response = yield call(UserService.updateUser, {
+            first_name: form_first_name,
+            last_name: form_last_name
+        });
+        const { first_name, last_name } = yield response.data;
+        yield put({ type: 'UPDATE_USER_SUCCESS', payload: { first_name, last_name } });
+        toastr.success("Update successful");
+        setSubmitting(false);
+        resetForm();
+        yield put(push("/"))
+    } catch (error) {
+        setSubmitting(false);
+        if (error.response && error.response.status) {
+            if (error.response.status === 400) {
+              if (error.response.data) {
+                if (error.response.data.detail) toastr.error(error.response.data.detail, { timeOut: 10000 });
+                setErrors(error.response.data);
+                isTouched = true;
+              }
+            } else {
+                yield call(handleApiErrorSaga, error);
+            }
+        } else {
+            yield call(handleErrorSaga, error);
+        }
     }
 }
 
-// Watcher
-function* listenForFetchUser() {
-    yield takeEvery('FETCH_USER', handleFetchUserSaga);
+function* listenForUpdateUserRequest() {
+    yield takeLatest('UPDATE_USER_REQUEST', udpateUser);
 }
 
 function* userSaga() {
     yield all([
-        call(listenForFetchUser),
-    ])
+        call(listenForUpdateUserRequest),
+    ]);
 }
 
 export default userSaga;
