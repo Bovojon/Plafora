d31521db71d369ce0af86cba4d248a8a7fffc597
Bovojon <bovojon@gmail.com>
2020-09-15 15:00:37 -0700

    Add CSS imports to main index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import store from './store';
-import { Provider } from 'react-redux';
+
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
