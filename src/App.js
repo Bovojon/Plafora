f57c59d9c732773e73626992a5084e429dd5e499
Bovojon <bovojon@gmail.com>
2020-09-15 17:11:41 -0700

    Small cleanups

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Switch, Route} from 'react-router-dom';
 import {ConnectedRouter} from 'connected-react-router';
 import ReduxToastr from 'react-redux-toastr';
 
-import './App.scss';
 import {history} from './store';
 import Home from './components/screens/Home';
 import Navbar from './components/nav/Navbar';
@@ -16,25 +15,34 @@ import RequestConfirmation from './components/screens/RequestConfirmation';
 import NotFound from './components/screens/NotFound';
 import ScrollToTop from './components/misc/ScrollToTop';
 
+import './App.scss';
+
 function App() {
   return (
-      <div className="App">
-        <ReduxToastr timeOut={4000} transitionIn="fadeIn" transitionOut="fadeOut" />
-        <ConnectedRouter history={history}>
-          {/* <ScrollToTop /> */}
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/places" component={Spaces} />
-            <Route exact path="/place/:id" component={Space} />
-            <Route exact path="/request/:id/confirmation" component={RequestConfirmation} />
-            <Route component={NotFound} />
-          </Switch>
-        </ConnectedRouter>
-      </div>
+      <>
+        <div className="App">
+          <ReduxToastr timeOut={4000} transitionIn="fadeIn" transitionOut="fadeOut" />
+          <ConnectedRouter history={history}>
+            <ScrollToTop />
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/places" component={Spaces} />
+              <Route exact path="/place/:id" component={Space} />
+              <Route exact path="/request/:id/confirmation" component={RequestConfirmation} />
+              <Route component={NotFound} />
+            </Switch>
+          </ConnectedRouter>
+        </div>
+        <footer>
+          <div className="text-muted mb-2">
+            Plafora
+          </div>
+        </footer>
+      </>
   );
 }
 
